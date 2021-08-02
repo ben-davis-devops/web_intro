4c3bb39fc92c9c1298e0e2b625718baba12b989b
Ben Davis <ben.david.davis16@gmail.com>
2021-08-02 16:12:35 +0100

    using forms

--- app.rb
+++ app.rb
@@ -17,8 +17,13 @@ get '/porsche' do
   "nice choice, great car!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
   erb(:index)
 end
 
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
