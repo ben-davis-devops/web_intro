11acb0862829465baa754cbbe0c499255dbc73d0
Ben Davis <ben.david.davis16@gmail.com>
2021-08-03 10:00:25 +0100

    up to CH16

--- app.rb
+++ app.rb
@@ -19,11 +19,15 @@ end
 
 get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
-  erb(:index)
+  erb :index
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
-  erb(:index)
+  erb :index
+end
+
+get '/cat-form' do
+  erb :cat_form
 end
